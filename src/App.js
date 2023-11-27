437b899d757d4bbc067732ac04ea8736a2726f7a
satyacasm <202151141@iiitvadodara.ac.in>
2023-11-27 12:06:41 +0530

    Integrated Smart Contracts with frontend

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Cards from './components/Cards';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
-import FeedbackForm from './components/Feedback';
-
+import FeedbackForm from './components/Feedback'; 
+// from interact.js
+import {main,balance,deposit,withdrawal,transfer,getAllTransactions,findBalance} from './interact';
 function App() {
   return (
     <div>
